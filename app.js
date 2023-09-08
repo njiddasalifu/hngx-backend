9b5f997ca5949b8e1a23570ba5c529bb94c29756
njiddasalifu <njiddasalifu@gmail.com>
2023-09-08 16:04:08 +0100

    formating utc

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const app = express();
 app.get('/salif/api', (req, res) => {
   const slackName = req.query.slack_name;
   const track = req.query.track;
+  
   // Defining the current day using the date format method
   const options = { weekday: 'long' };
   const currentDay = new Date().toLocaleDateString('en-US', options);
   
-  // Getting the current UTC time in the desired format
-  const utcTime = new Date().toISOString();
+  // Getting the current UTC time and formatting it in the desired format
+  const utcTime = new Date().toISOString().replace(/[:\-]|\.\d{3}/g, '');
 
   const githubFileUrl = req.query.github_file_url;
   const githubRepoUrl = req.query.github_repo_url;
@@ -18,7 +19,7 @@ app.get('/salif/api', (req, res) => {
     slack_name: "HNGx",
     track: "backend",
     current_day: currentDay,
-    utc_time: utcTime,
+    utc_time: utcTime + 'Z', // Appending 'Z' to indicate UTC timezone
     github_file_url: "https://github.com/njiddasalifu/hngx-backend/blob/main/app.js",
     github_repo_url: "https://github.com/njiddasalifu/hngx-backend",
     status_code: 200
