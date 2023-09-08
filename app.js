1051877da4621bdb43272d38c27ca6ca1987e69e
njiddasalifu <njiddasalifu@gmail.com>
2023-09-08 15:05:22 +0100

    processing from .env

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.get('/salif/api', (req, res) => {
   res.json(response);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running...');
 });
