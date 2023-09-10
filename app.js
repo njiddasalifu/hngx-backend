5028872f06309a77b6bf81188ef2be8dbbb7c16a
Njidda Salifu <njiddasalifu@gmail.com>
2023-09-10 11:15:28 +0100

    Update app.js
    Using port 5000

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get('/salif/api', (req, res) => {
   res.json(response);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running...');
 });
